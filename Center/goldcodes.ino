3c23199b0c848e4f38c8825bc9d9bfb4b6eeebb4
Daphne Poon <dpoon@hmc.edu>
2017-11-17 09:47:18 -0800

    thursday 16/11 edits
    yay!

--- Center/goldcodes.ino
+++ Center/goldcodes.ino
@@ -1,6 +1,6 @@
 // credits to Yaqub :))
-#define LED1 13 //Inside
-#define LED2 5 //Outside
+int LED1 = 13; //Inside
+int LED2 = 5; //Outside
 boolean invertBits = false;
 boolean goldCodes[9][31] = {{0,0,0,0,0,0,0,1,0,0,0,1,1,0,1,1,0,0,0,0,1,1,0,0,1,1,1,0,0,1,1}, 
                            {1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0,0,0,1,1,1,1,0,0,0,1,0,1,0,0}, 
@@ -22,69 +22,80 @@ void gcsetup(){
   digitalWrite(LED2, LOW);
   //delay(1500);
   invertBits = !(digitalRead(3));
+  if (clockwise == true){
+    int LED1 = 5; //switch them, so that the right leds flash the right code
+    int LED2 = 13; 
+  }
 }
 
 void gcloop() { 
   if (invertBits == false){
     for(int i=4; i<8; i++)
-    {
-      for(int z=0; z<31; z++)
+    { 
+      for(int y=0; y<2; y++) //loops gc twice
       {
-          long startTime = micros();
-          while(micros() - startTime <= 250)
-          {
-           if (goldCodes[i][z] == 1) 
-                {
-                  digitalWrite(LED1, HIGH);
-                  if (goldCodes[8][z] == 1){
-                    digitalWrite(LED2, HIGH);
+        for(int z=0; z<31; z++)
+        {
+            long startTime = micros();
+            while(micros() - startTime <= 250)
+            {
+             if (goldCodes[i][z] == 1) 
+                  {
+                    digitalWrite(LED1, HIGH);
+                    if (goldCodes[8][z] == 1){
+                      digitalWrite(LED2, HIGH);
+                    }
+                    else{
+                      digitalWrite(LED2, LOW);
+                    }
                   }
-                  else{
-                    digitalWrite(LED2, LOW);
+             else
+                  {
+                    digitalWrite(LED1, LOW);
+                    if (goldCodes[8][z] == 1){
+                      digitalWrite(LED2, HIGH);
+                    }
+                    else{
+                      digitalWrite(LED2, LOW);
+                    }
                   }
-                }
-           else
-                {
-                  digitalWrite(LED1, LOW);
-                  if (goldCodes[8][z] == 1){
-                    digitalWrite(LED2, HIGH);
-                  }
-                  else{
-                    digitalWrite(LED2, LOW);
-                  }
-                }
+            }
           }
-        }
       }
+    }
   }
+
   else{
     for(int i=4; i<8; i++)
     {
-      for(int z=0; z<31; z++)
+      for(int y=0; y<2; y++) //loops gc twice
       {
-          long startTime = micros();
-          while(micros() - startTime <= 250)
-          {
-           if (goldCodes[i][z] == 1) 
-                {
-                  digitalWrite(LED1, LOW);
-                  if (goldCodes[8][z] == 1){
-                    digitalWrite(LED2, LOW);
+        for(int z=0; z<31; z++)
+        {
+            long startTime = micros();
+            while(micros() - startTime <= 250)
+            {
+             if (goldCodes[i][z] == 1) 
+                  {
+                    digitalWrite(LED1, LOW);
+                    if (goldCodes[8][z] == 1){
+                      digitalWrite(LED2, LOW);
+                    }
+                    else{
+                      digitalWrite(LED2, HIGH);
+                    }
                   }
-                  else{
-                    digitalWrite(LED2, HIGH);
+             else
+                  {
+                    digitalWrite(LED1, HIGH);
+                    if (goldCodes[8][z] == 1){
+                      digitalWrite(LED2, LOW);
+                    }
+                    else{
+                      digitalWrite(LED2, HIGH);
+                    }
                   }
-                }
-           else
-                {
-                  digitalWrite(LED1, HIGH);
-                  if (goldCodes[8][z] == 1){
-                    digitalWrite(LED2, LOW);
-                  }
-                  else{
-                    digitalWrite(LED2, HIGH);
-                  }
-                }
+            }
           }
         }
       }
